f92f46fdb03277ec3351e6d7d7a91f33d4fdcc91
grandiloquent <1785907865@qq.com>
2021-06-02 00:48:40 +0800

    changed

--- index.js
+++ index.js
@@ -192,11 +192,48 @@ _overlay.addEventListener('click', event => {
 
 
 
-document.querySelector('#copy').addEventListener('click', event => {
+document.getElementById('copy').addEventListener('click', event => {
     if (_video.src) {
         writeText(_video.src);
         _toast.setAttribute('message', '成功复制到剪切板');
     }
+
+});
+
+
+document.getElementById('help').addEventListener('click', event => {
+    event.stopPropagation();
+    event.preventDefault();
+    window.location.href = '/help';
+});
+
+document.getElementById('volume').addEventListener('click', event => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    _video.muted = !_video.muted;
+
+    if (_video.muted) {
+        event.currentTarget.setAttribute('d', "M12 3.984v4.219l-2.109-2.109zM4.266 3l16.734 16.734-1.266 1.266-2.063-2.063q-1.547 1.313-3.656 1.828v-2.063q1.172-0.328 2.25-1.172l-4.266-4.266v6.75l-5.016-5.016h-3.984v-6h4.734l-4.734-4.734zM18.984 12q0-2.391-1.383-4.219t-3.586-2.484v-2.063q3.047 0.656 5.016 3.117t1.969 5.648q0 2.203-1.031 4.172l-1.5-1.547q0.516-1.266 0.516-2.625zM16.5 12q0 0.422-0.047 0.609l-2.438-2.438v-2.203q1.031 0.516 1.758 1.688t0.727 2.344z");
+        event.currentTarget.setAttribute('name', '静音');
+    } else {
+        event.currentTarget.setAttribute('d', "M14.016 3.234q3.047 0.656 5.016 3.117t1.969 5.648-1.969 5.648-5.016 3.117v-2.063q2.203-0.656 3.586-2.484t1.383-4.219-1.383-4.219-3.586-2.484v-2.063zM16.5 12q0 2.813-2.484 4.031v-8.063q1.031 0.516 1.758 1.688t0.727 2.344zM3 9h3.984l5.016-5.016v16.031l-5.016-5.016h-3.984v-6z");
+        event.currentTarget.setAttribute('name', "声音");
+    }
+});
+
+document.getElementById('download').addEventListener('click', event => {
+    event.stopPropagation();
+    event.preventDefault();
+    if (!_video.src) return;
+    const a = document.createElement('a');
+    a.href = _video.src;
+    a.setAttribute('download', 'download');
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+
 });
 // -------------------
 
@@ -314,25 +351,3 @@ function renderYouTube(obj) {
     }
     app.appendChild(div);
 }
-
-
-document.getElementById('help').addEventListener('click', event => {
-    event.stopPropagation();
-    event.preventDefault();
-    window.location.href = '/help';
-});
-
-document.getElementById('volume').addEventListener('click', event => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    _video.muted = !_video.muted;
-
-    if (_video.muted) {
-        event.currentTarget.setAttribute('d', "M12 3.984v4.219l-2.109-2.109zM4.266 3l16.734 16.734-1.266 1.266-2.063-2.063q-1.547 1.313-3.656 1.828v-2.063q1.172-0.328 2.25-1.172l-4.266-4.266v6.75l-5.016-5.016h-3.984v-6h4.734l-4.734-4.734zM18.984 12q0-2.391-1.383-4.219t-3.586-2.484v-2.063q3.047 0.656 5.016 3.117t1.969 5.648q0 2.203-1.031 4.172l-1.5-1.547q0.516-1.266 0.516-2.625zM16.5 12q0 0.422-0.047 0.609l-2.438-2.438v-2.203q1.031 0.516 1.758 1.688t0.727 2.344z");
-        event.currentTarget.setAttribute('name', '静音');
-    } else {
-        event.currentTarget.setAttribute('d',"M14.016 3.234q3.047 0.656 5.016 3.117t1.969 5.648-1.969 5.648-5.016 3.117v-2.063q2.203-0.656 3.586-2.484t1.383-4.219-1.383-4.219-3.586-2.484v-2.063zM16.5 12q0 2.813-2.484 4.031v-8.063q1.031 0.516 1.758 1.688t0.727 2.344zM3 9h3.984l5.016-5.016v16.031l-5.016-5.016h-3.984v-6z");
-        event.currentTarget.setAttribute('name',"声音");
-    }
-});
