e14d5566d881ab94d2522284afb21ed922775e21
grandiloquent <1785907865@qq.com>
2022-05-22 11:34:04 +0800

    Update

--- index/index.js
+++ index/index.js
@@ -1,11 +1,10 @@
 const video = document.querySelector('video');
 const player = document.querySelector('.player');
 
-video.muted = true;
-video.autoplay = true;
 
 video.addEventListener('durationchange', ev => {
     const ratio = video.videoHeight / video.videoWidth;
     player.style.width=`${window.innerWidth}px`;
     player.style.height = `${window.innerWidth* ratio}px`;
-})
+});
+video.play();
